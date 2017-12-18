e1f1caefc12962804b86104901d4bac1d486bb09
manpoe <manuel.poelzl28@gmail.com>
2017-12-18 14:21:26 +0100

    Basic structure

---
+++ PHP/RestaurantView.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+  <title>Overview</title>
+  <!-- Libraries -->
+  <script src="../Libraries/jQuery/jQuery.js"></script>
+  <script src="../Libraries/jQueryDataTables/datatables.js"></script>
+  <script src="../Libraries/jQueryUI/jquery-ui.js"></script>
+  <script src="../Libraries/Materialize/materialize.js"></script>
+  <script src="../Libraries/Own/JLGF.js"></script>
+  <!-- Scripts -->
+  <script src="../JavaScript/Scripts/General.js"></script>
+  <!-- CSS -->
+  <link rel="stylesheet" href="../CSS/jquery-ui.css">
+  <link rel="stylesheet" href="../CSS/datatables.css">
+  <link rel="stylesheet" href="../CSS/materialize.css">
+  <link rel="stylesheet" href="../CSS/General.css">
+
+  <link rel='stylesheet' href='//fonts.googleapis.com/css?family=font1|font2|etc' type='text/css'>
+</head>
+
+<body id="ResView_Body">
+  <ul id="sideNavigator" class="side-nav">
+
+
+  </ul>
+  <a href="#" id="ResView_SideNavActivator" data-activates="sideNavigator" class="button-collapse"><i class="material-icons">menu</i></a>
+</body>
+
+</html>
