8cb162f80a8ae4ee802224c9ada7ff8613d3bc94
manpoe <manuel.poelzl28@gmail.com>
2017-12-20 13:27:08 +0100

    Implemented alternative query to update users

--- PHPScripts/Register.php
+++ PHPScripts/Register.php
@@ -1,34 +1,35 @@
 <?php
-    $firstname = $_POST["textfield_firstname"];
-    $lastname = $_POST["textfield_lastname"];
-    $username = $_POST["textfield_username"];
-    $password = $_POST["textfield_password"];
-    $hashed = hash('SHA256', $password);
-    $isX = $_POST["isX"];
-    $isRestaurant = 0;
-    $isParent = 0;
-    $isDayCare = 0;
+$firstname = $_POST["textfield_firstname"];
+$lastname = $_POST["textfield_lastname"];
+$username = $_POST["textfield_username"];
+$password = $_POST["textfield_password"];
+$hashed = hash('SHA256', $password);
+$isX = $_POST["isX"];
+$isRestaurant = 0;
+$isParent = 0;
+$isDayCare = 0;
 
-    //Prüft welcher der Radiobuttons ausgewählt wurde
-    if ($isX == 'isRestaurant') {
-        $isRestaurant == 1;
-    }
-    if ($isX == 'isDayCare') {
-        $isDayCare = 1;
-    }
-    if ($isX == 'isParent') {
-        $isParent == 1;
-    }
+//Prüft welcher der Radiobuttons ausgewählt wurde
+if ($isX == 'isRestaurant') {
+    $isRestaurant == 1;
+}
+if ($isX == 'isDayCare') {
+    $isDayCare = 1;
+}
+if ($isX == 'isParent') {
+    $isParent == 1;
+}
 
-    else{
-        require_once("../PHPClasses/Helper.php");
-        $DatabaseHelper=new DatabaseHelper();
-        
-        $connection=$DatabaseHelper->Connect("localhost","root","poelzlpichler_gr04!","meal_management");
-        $getLatesIDDataQuery="SELECT max(id) FROM tb_users";
-        $LatestID=$DatabaseHelper->Query($connection,$getLatestIDDataQuery);
-        $nextID = $LatestID + 1;
-        
-        $createUserDataQuery="INSERT INTO tb_users (id, firstname, lastname, username, password, isRestaurant, isDayCare, isParent) VALUES ('$id', '$firstname', '$lastname', '$username', '$hashed', '$isRestaurant', '$isDayCare', '$isParent')";
-        $createUser=$DatabaseHelper->Query($connection,$createUserDataQuery);
-    }
+else{
+    require_once("../PHPClasses/Helper.php");
+    $DatabaseHelper=new DatabaseHelper();
+    
+    $connection=$DatabaseHelper->Connect("localhost","root","poelzlpichler_gr04!","meal_management");
+    $getLatesIDDataQuery="SELECT max(id) FROM tb_users";
+    $LatestID=$DatabaseHelper->Query($connection,$getLatestIDDataQuery);
+    $nextID = $LatestID + 1;
+    
+    $createUserDataQuery="INSERT INTO tb_users (id, firstname, lastname, username, password, isRestaurant, isDayCare, isParent) VALUES ('$id', '$firstname', '$lastname', '$username', '$hashed', '$isRestaurant', '$isDayCare', '$isParent')";
+    // $createUserDataQuery="INSERT INTO tb_users VALUES ('$id', '$firstname', '$lastname', '$username', '$hashed', '$isRestaurant', '$isDayCare', '$isParent')"; ALTERNATIVE QUERY
+    $createUser=$DatabaseHelper->Query($connection,$createUserDataQuery);
+}
